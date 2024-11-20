3032562936ddeefa828189da5ed33d03f7390d0b
Mohammed-glr <102376@glr.nl>
2024-11-20 11:22:16 +0000

    dfg

--- main.c
+++ main.c
@@ -1,12 +1,44 @@
 #include <stdio.h>
-
-int plus(int a, int b) {
+double plus(double a, double b) {
     return a + b;
 }
 
-int main() {
-    int sum = plus(1, 65);
-    printf("%d\n", sum);
+
+double minus(double a, double b){
+    return a - b;
+}
+double divide(double a,double b){
+    if (a == 0 || b ==0){
+        return 0;
+    }
+
+    return a / b;
+
+}
+double times (double a ,double b){
+    return a * b ;
+}
+
+int main(int argc, char* argv[]) {
+    for (int i =0; i < 4; i++){
+        printf("%s\n", argv[i]);
+    }
+
+    
+
+    double keer = times(10,10);
+    printf("%f\n",keer);
+
+    double deel = divide(10,15);
+    printf("%f\n", deel);
+
+    double min = minus(0,0);
+    printf("%f\n", min);
+
+    double sum = plus(1, 65);
+    printf("%f\n", sum);
     return 1;
+
+
 }
 
