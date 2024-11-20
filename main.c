de6416f155b626ad764eb2c07306790a00bcf41e
Mohammed-glr <102376@glr.nl>
2024-11-20 19:06:16 +0000

    Calculator is done

--- main.c
+++ main.c
@@ -1,44 +1,35 @@
-#include <stdio.h>
-double plus(double a, double b) {
-    return a + b;
-}
+#include<stdio.h>
 
+int main(){
+    char operator;
+    double first, second;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
 
-double minus(double a, double b){
-    return a - b;
-}
-double divide(double a,double b){
-    if (a == 0 || b ==0){
-        return 0;
-    }
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
 
-    return a / b;
-
-}
-double times (double a ,double b){
-    return a * b ;
-}
-
-int main(int argc, char* argv[]) {
-    for (int i =0; i < 4; i++){
-        printf("%s\n", argv[i]);
-    }
-
+    switch (operator)
+    {
+    case '+':
+        printf("%lf + %lf = %lf\n", first, second, first + second);
+        break;
+    case '-':
+        printf("%lf - %lf = %lf\n", first, second, first - second);
+        break;
+    case '*':
+        printf("%lf * %lf = %lf\n", first, second, first * second);
+        break;
+    case '/':
+        if (second != 0.0)
+            printf("%lf / %lf = %lf\n", first, second, first / second);
+        else
+            printf("Error! Division by zero.\n");
+        break;    ;
     
+    default:
+        printf("Error! operator is not correct");
+    }
 
-    double keer = times(10,10);
-    printf("%f\n",keer);
-
-    double deel = divide(10,15);
-    printf("%f\n", deel);
-
-    double min = minus(0,0);
-    printf("%f\n", min);
-
-    double sum = plus(1, 65);
-    printf("%f\n", sum);
-    return 1;
-
-
+    return 0;
 }
-
